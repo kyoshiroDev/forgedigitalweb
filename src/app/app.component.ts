29bc1d2fbcdfcf33f0639ee1d3b3c434b0268fe0
greg <tahir.gregory.dev@gmail.com>
2024-08-26 06:52:10 +0200

    correction bug menu burger sur affichage des icines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ import {FooterComponent} from "./components/footer/footer.component";
     ContactComponent,
     FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Forge Digital Web';
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,5 @@
 import {Component, Input,} from '@angular/core';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faBars} from "@fortawesome/free-solid-svg-icons/faBars";
-import {faXmark} from "@fortawesome/free-solid-svg-icons/faXmark";
 import {NgClass, NgOptimizedImage} from "@angular/common";
 import {Title} from "@angular/platform-browser";
 
@@ -18,9 +16,6 @@ interface onInit {
 
 export class HeaderComponent {
   @Input('title') title: string = '';
-
-  faBars = faBars;
-  faXmark = faXmark;
 
   // Mobile menu activation
   active: boolean = false;
