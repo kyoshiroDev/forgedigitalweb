42d22eb23361f987ae34a4b615b909c250413456
greg <tahir.gregory.dev@gmail.com>
2024-08-27 19:09:31 +0200

    essaie pour submit le formulaire

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ContactModel } from '../../models/contact-model';
 
 @Component({
